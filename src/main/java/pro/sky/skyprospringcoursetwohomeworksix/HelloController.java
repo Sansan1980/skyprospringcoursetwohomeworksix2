a3300b332ea88c53dd06132f2c85e499d0057130
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2024-04-09 15:17:08 +0300

    Initiall commit

---
+++ src/main/java/pro/sky/skyprospringcoursetwohomeworksix/HelloController.java
@@ -0,0 +1,12 @@
+package pro.sky.skyprospringcoursetwohomeworksix;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+public String hello() {
+        return "HHHHHHello";
+    }
+}
---
+++ src/main/java/pro/sky/skyprospringcoursetwohomeworksix/SkyproSpringCoursetwohomeworksixApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcoursetwohomeworksix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCoursetwohomeworksixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCoursetwohomeworksixApplication.class, args);
+	}
+
+}
