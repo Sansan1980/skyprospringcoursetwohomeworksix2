ef082e851985eb28b83f19c796807df9f190b481
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2024-04-09 18:27:49 +0300

    Сделал заново

---
+++ src/test/java/pro/sky/skyprospringcoursetwohomeworksix/SkyproSpringCoursetwohomeworksixApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcoursetwohomeworksix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringCoursetwohomeworksixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
